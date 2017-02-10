e20050a914d5a7daec7848efdcb4fefd9b40d998
Olivia Powell <opowell6@gatech.edu>
2017-02-10 18:51:53 -0500

    modified file structure tree

--- app/src/main/java/com/example/olivia/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/olivia/myapplication/SplashActivity.java
@@ -14,7 +14,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
