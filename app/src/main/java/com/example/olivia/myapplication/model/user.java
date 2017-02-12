bba49400ba8c02ff4b16d2a834e5395e87d3a572
Olivia Powell <opowell6@gatech.edu>
2017-02-12 12:43:17 -0500

    added UserManager and facades to model

--- app/src/main/java/com/example/olivia/myapplication/model/user.java
+++ app/src/main/java/com/example/olivia/myapplication/model/user.java
@@ -4,5 +4,17 @@ package com.example.olivia.myapplication.model;
  * Created by Olivia on 2/12/2017.
  */
 
-public class user {
+public class User {
+    String name;
+    String password;
+
+    public User(String n, String p) {
+        name = n;
+        password = p;
+    }
+
+    public boolean checkPassword(String pass) {
+        return password.equals(pass);
+    }
+
 }
