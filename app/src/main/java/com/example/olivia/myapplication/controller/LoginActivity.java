24f09ee6a60dc8c7783e65c178ee51aad6da0e08
Julian Barnes <julian.barnes100@gmail.com>
2017-02-12 12:38:34 -0500

    Password field returns error if password is incorrect

--- app/src/main/java/com/example/olivia/myapplication/controller/LoginActivity.java
+++ app/src/main/java/com/example/olivia/myapplication/controller/LoginActivity.java
@@ -320,7 +320,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
