08493fb5417e801de7a101a7761934244a8ef541
Olivia Powell <opowell6@gatech.edu>
2017-02-13 09:48:08 -0500

    Added Logout functionality

--- app/src/main/java/com/example/olivia/myapplication/controller/DummyApp.java
+++ app/src/main/java/com/example/olivia/myapplication/controller/DummyApp.java
@@ -1,7 +1,7 @@
 package com.example.olivia.myapplication.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,19 +12,20 @@ import android.widget.Button;
 
 public class DummyApp extends AppCompatActivity {
 
-    private Button closeButton;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dummy_app);
 
-        closeButton = (Button) findViewById(R.id.close_button);
-        closeButton.setOnClickListener(new View.OnClickListener() {
+        logoutButton = (Button) findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(DummyApp.this, LoginActivity.class);
+                startActivity(intent);
                 finish();
-                System.exit(0);
             }
         });
     }
